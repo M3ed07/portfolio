12ae2d63d5a85e13439ddbb73ece66b9cbe382d5
melad duraid <meladhero4@gmail.com>
2024-06-01 18:29:22 +0300

    add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
